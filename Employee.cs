78cbdc9f8170a753de5afd7937dee21bb4ea4867
Kealiwyn <67714527+Kealiwyn@users.noreply.github.com>
2021-03-22 11:26:14 -0400

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    class Employee
+    {
+        private int employeeUserId;
+        private string firstName;
+        private string lastName;
+        private double basicSalary;
+        private double grossSales;
+        private double commissionRate;
+
+        public int EmployeeUserId { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double BasicSalary { get; set; }
+        public double GrossSales { get; set; }
+        public double CommissionRate { get; set; }
+
+        public Employee() {
+            this.CommissionRate = 0.2;
+            this.basicSalary = 2000.0;
+        }
+
+        public Employee(int employeeUserId, string firstName, double basicSalary) { }
+
+        public double Earnings()
+        {
+                if (GrossSales <= 0)
+                {
+                    Console.Write("Your gross sales should be higher than zero. Please try again: ");
+                    GrossSales = Convert.ToDouble(Console.ReadLine());
+                    Earnings();
+                }
+                else if (CommissionRate < 0.1 || CommissionRate > 1.0)
+                {
+                    Console.Write("Your commission rate should be between 0.1 and 1.0. Please try again: ");
+                    CommissionRate = Convert.ToDouble(Console.ReadLine());     
+                    Earnings();
+                }
+            double EmployeeComission = CommissionRate * GrossSales + BasicSalary; 
+            return EmployeeComission;
+        }
+    }
+}
---
+++ EmployeeTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    class EmployeeTest
+    {
+        static void Main(string[] args)
+        {
+            Employee firstEmployee = new Employee(002, "bob", 20);
+
+            Employee secondEmployee = new Employee();
+            Console.Write("Please input your first name: ");
+            secondEmployee.FirstName = Console.ReadLine();
+            Console.Write("Please input your last name: ");
+            secondEmployee.LastName = Console.ReadLine();
+            Console.Write("Please input your base salary: ");
+            secondEmployee.BasicSalary = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please input your gross sales: ");
+            secondEmployee.GrossSales = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please input your commission rate: ");
+            secondEmployee.CommissionRate = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write($"Your employee commission is: {secondEmployee.Earnings():c2}");
+        }
+    }
+}
